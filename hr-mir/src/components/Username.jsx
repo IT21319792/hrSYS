cddc3af6f8ec692695b30d1538803a602b88970f
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-31 14:03:02 +0530

    Create Username.jsx

---
+++ hr-mir/src/components/Username.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Username() {
+  return (
+   <div className='container mx-auto'>
+    <div className='flex justify-center items-center h-screen'>
+      <div>
+        <div className='title flex flex-col items-center'>
+          <h1 className='text-3xl font-bold'>Username</h1>
+          <span className='py-4 text-xl,w=2/3 text-center text-gray-500'>Enter your username</span>
+
+        </div>
+      </div>
+
+    </div>
+   </div>
+  )
+}
