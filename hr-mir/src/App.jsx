3d7d9f7c52e2d04066319f4f88329365bd095177
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-31 12:38:38 +0530

    Update App.jsx

--- hr-mir/src/App.jsx
+++ hr-mir/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+
+ const  router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Root Route</div>
+  },
+  {
+    path: '/register',
+    element: <div>Register</div>
+  }
+ ])
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <main>
+      <RouterProvider router={router}>
+        <div>
+          <h1>App</h1>
+        </div>
+      </RouterProvider>
+
+    </main>
   )
 }
