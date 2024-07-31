6948b4a3dd688d8987c85d9beea3e70df7c74484
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-07-31 14:02:45 +0530

    Update App.jsx

--- hr-mir/src/App.jsx
+++ hr-mir/src/App.jsx
@@ -2,15 +2,46 @@ import React from 'react'
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 
+// import all components
+import Username from './components/Username'
+import Password from './components/Password'
+import Profile from './components/Profile'
+import Register from './components/Register'
+import Reset from './components/Reset'
+import Recovery from './components/Recovery'
+import PageNotFound from './components/PageNotFound'
+
  const  router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Root Route</div>
+    element: <Username></Username>
   },
   {
     path: '/register',
-    element: <div>Register</div>
+    element: <Register></Register>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
   }
+
+
  ])
 
 export default function App() {
