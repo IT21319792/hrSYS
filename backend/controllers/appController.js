a7e1f839fa3fc349555ea25ec9dfbdfd1e64c4c0
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-08-07 10:51:23 +0530

    Update appController.js

--- backend/controllers/appController.js
+++ backend/controllers/appController.js
@@ -1,70 +1,103 @@
 
 import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
+// example json object for register
+// {
+//     "username": "test",
+//     "password": "test",
+//     "email": "test@gmail.com",
+//     "firstName": "test",
+//     "lastName": "test",
+//     "mobile": "123123123",
+//     "address": "test",
+//     "profile": "test"
+// }
+
 
 export async function register(req, res) {
     try {
-        const {username, password , profile, email}=req.body;
-        //check the existence of the user
-        const existUsername = new Promise ((resolve, reject) => {
-       UserModel.findOne({username},function(err, user){
-           if(err) reject(new Error(err));
-           if(user) reject({error: "Username already exists"});
-              resolve();
-         });
-        });
+        const { username, password, profile, email } = req.body;
 
-        //check the existence of the email
-        const existEmail = new Promise ((resolve, reject) => {
-            UserModel.findOne({email},function(err, email){
-                if(err) reject(new Error(err));
-                if(email) reject({error: "Email already exists"});
-                   resolve();
-              });
-             });
+        // Check the existence of the username
+        const existUsername = UserModel.findOne({ username }).exec();
 
-    Promise.all([existUsername, existEmail]).then(() => {
-        if(password){
-            bcrypt.hash(password,10)
-                .then(hashedPassword => {
-                    const user = new UserModel({
-                        username,
-                        password: hashedPassword,
-                        email,
-                        profile
-                    });
-                    user.save()
-                    .then(() => {
-                        return res.status(201).send({message: "User registered successfully"});
-                    })
-                    .catch(error => {
-                        return res.status(500).send({
-                            error: "Enable to save user"
-                        })
-                    })
+        // Check the existence of the email
+        const existEmail = UserModel.findOne({ email }).exec();
 
-                }).catch(error => {
-                    return res.status(500).send({
-                        error: "Enable to hashed password"
-                    })
+        const [user, emailRecord] = await Promise.all([existUsername, existEmail]);
 
-                }
-            )
-
+        if (user) {
+            return res.status(400).send({ error: "Username already exists" });
         }
-    }).catch(error => {
-        return res.status(500).send({error})
 
-    });
+        if (emailRecord) {
+            return res.status(400).send({ error: "Email already exists" });
+        }
 
+        if (password) {
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Create and save the new user
+            const newUser = new UserModel({
+                username,
+                password: hashedPassword,
+                email,
+                profile: profile || '',
+            });
+
+            await newUser.save();
+
+            return res.status(201).send({ message: "User registered successfully" });
+        } else {
+            return res.status(400).send({ error: "Password is required" });
+        }
     } catch (error) {
-        return res.status(500).send(error);
+        return res.status(500).send({ error: error.message });
     }
-    }
+}
+
+
+
 
     
+ //login   
 export async function login(req, res) {
-    res.json('login post req');
+   const { username, password } = req.body;
+   try {
+    UserModel.findOne({username})
+    .then(user => {
+        bcrypt.compare(password, user.password)
+        .then(passwordCheck => {
+            if(!passwordCheck) {
+                return res.status(400).send({ error: "dont have password" });
+
+                //create token
+              const token=  jwt.sign({
+                    userId: user._id,
+                    username: user.username,
+                    
+                }, 'secret',{expiresIn:'1h'});
+
+                return res.status(200).send({message: "Login successful", token: token, username: user.username});
+
+            }
+        })
+        .catch(error => {
+            return res.status(400).send({ error: "Password is incorrect" });
+        });
+    }
+    )
+    .catch(error => {
+        return res.status(404).send({ error: "Username not found" });
+    });
+    
+   } catch (error) {
+    return res.status(500).send({ error: error.message });
+   }
     }
 
 
