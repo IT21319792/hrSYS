543c87e417b8ee852b6178527dc169553f319654
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-08-07 11:38:10 +0530

    Update route.js

--- backend/router/route.js
+++ backend/router/route.js
@@ -9,7 +9,7 @@ import * as controller from '../controllers/appController.js';
 router.route('/register').post(controller.register);
 // router.route('/registerMail').post((req, res) => res.json('registerMail post req'));
 router.route('/authenticate').post((req, res) => res.end());
-router.route('/login').post(controller.login);
+router.route('/login').post(controller.verifyUser,controller.login);
 
 
 //get method
