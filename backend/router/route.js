3ed58ed1019e2c2433438addbededa8cf0e7b452
Boshitha Gunarathna <IT21319792@my.sliit.lk>
2024-08-08 11:10:05 +0530

    Update route.js

--- backend/router/route.js
+++ backend/router/route.js
@@ -5,24 +5,24 @@ const router = Router();
 
 // import all controllers
 import * as controller from '../controllers/appController.js';
-import Auth from "../middleware/auth.js";
+import Auth , {localVariables} from "../middleware/auth.js";
 
 //post method
 router.route('/register').post(controller.register);
-// router.route('/registerMail').post((req, res) => res.json('registerMail post req'));
+router.route('/registerMail').post((req, res) => res.json('registerMail post req'));
 router.route('/authenticate').post((req, res) => res.end());
 router.route('/login').post(controller.verifyUser,controller.login);
 
 
 //get method
 router.route('/user/:username').get(controller.getUser);
-router.route('/generateOTP').get(controller.generateOTP);
+router.route('/generateOTP').get( controller.verifyUser, localVariables,controller.generateOTP);
 router.route('/verifyOTP').get(controller.verifyOTP);
 router.route('/createReset').get(controller.createResetSession); 
 
 
 //put method
-router.route('/resetPassword').put( controller.resetPassword);
+router.route('/resetPassword').put( controller.verifyUser,controller.resetPassword);
 router.route('/updateUser').put(Auth,controller.updateUser);
 
 
